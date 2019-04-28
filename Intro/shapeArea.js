e66e3f0e1a38d05117e9a9ef28f39f100eb1e9fc
Paul Jerome <mr.paulthomasjerome@gmail.com>
2019-04-28 15:21:50 -0700

    Create shapeArea.js

---
+++ Intro/shapeArea.js
@@ -0,0 +1,18 @@
+/*
+Find the area of a polygon for a given n.
+A 1-interesting polygon is just a square with a side of length 1. 
+An n-interesting polygon is obtained by taking the n - 1-interesting 
+polygon and appending 1-interesting polygons to its rim, side by side.
+*/
+
+let shapeArea = n => {
+  let result = 0;
+
+  if(n == 1) {
+    result = 1;
+  } 
+  else if(n > 1) {
+    result = shapeArea(n - 1) + 4 * (n - 1);
+  }
+  return result;
+}
