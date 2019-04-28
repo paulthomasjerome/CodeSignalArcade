030e44bd544c5e137c8f7c357e0844f3b1319efb
Paul Jerome <mr.paulthomasjerome@gmail.com>
2019-04-28 15:08:58 -0700

    Update centuryFromYear.js
    Changed formatting to be more consistent with other files

--- Intro/centuryFromYear.js
+++ Intro/centuryFromYear.js
@@ -5,23 +5,17 @@ the year 100, the second - from the year 101 up to and including the year 200, e
 
 let centuryFromYear = (year) => {
     result = 0;
-    if(year < 100 || year > 9)
-    {
-    result = 1;
-    }
+    
+    if(year < 100 || year > 9) result = 1;
 
-    if(year > 99)
-    {
-        if(year % 10 == 0)
-        {
+    if(year > 99) {
+        if(year % 10 == 0) {
             result = year / 100;
         }
-        else
-        {
+        else {
             result = year / 100 + 1;
         }
-
     }
-        result = Math.floor(result);
-        return result;
+    result = Math.floor(result);
+    return result;
 }
