16eac63117ee78f9bacd6b8161703fc72b5c130d
Paul Jerome <mr.paulthomasjerome@gmail.com>
2019-04-28 15:18:56 -0700

    Update syntax

--- Intro/adjacentElementsProduct.js
+++ Intro/adjacentElementsProduct.js
@@ -1,6 +1,6 @@
 //Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
 
-function adjacentElementsProduct(inputArray) {
+let adjacentElementsProduct = inputArray => {
   let max = inputArray[0] * inputArray[1];
   let current = 0;
   let subInputLength = inputArray.length - 1;
